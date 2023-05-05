79d43199c7b124d004390b4f1a4548369cf3be86
KostasEreksonas <k.ereksonas@gmail.com>
2023-05-05 20:55:19 +0300

    Add sharepoint domain name and site to config file

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 def create_config():
-    username = input("Enter username: ")
-    password = input("Enter password: ")
+    username = input("Enter Microsoft 365 username: ")
+    password = input("Enter Microsoft 365 password: ")
+    domain = input("Enter name of your sharepoint domain: ")
+    site = input("Enter name of your sharepoint site: ")
     with open('config.py', 'w') as file:
-        file.write(f'#!/usr/bin/env python3\n\nusername = \"{username}\"\npassword = \"{password}\"\n')
+        file.write(f'#!/usr/bin/env python3\n\nusername = \"{username}\"\npassword = \"{password}\"\ndomain = \"{domain}\"\nsite = \"{site}\"\n')
 
 create_config()
